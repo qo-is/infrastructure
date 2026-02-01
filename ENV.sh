44960e425668d9014b185b397401ce7fceef0200
Fabian Hauser <fabian@fh2.ch>
2026-02-01 12:02:33 +0200

    TMP VALUES

---
+++ ENV.sh
@@ -0,0 +1,4 @@
+export FAILED_DISK=/dev/disk/by-id/nvme-SAMSUNG_MZVL22T0HBLB-00B00_S677NE0NC01017_1
+export FAILED_PARTITION=${FAILED_DISK}-part2
+export REPLACEMENT_DISK=/dev/disk/by-id/usb-Realtek_USB_3.2_Device_012345679175-0:0
+export RAID_DEVICE=/dev/md/raid_system
