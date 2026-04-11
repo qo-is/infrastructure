4dd79f11d365a4a28978d8e848677606ea0d4338
Raphael Zimmermann <mail@raphael.li>
2026-04-11 10:17:35 +0200

    Add args option to pass additional args to tests

--- checks/nixos-modules/default.nix
+++ checks/nixos-modules/default.nix
@@ -24,43 +24,62 @@ let
     let
       getFilePath = file: path.append modulesBaseDir "./${name}/${file}";
     in
-    testers.runNixOSTest {
-      inherit name;
+    testers.runNixOSTest (
+      { config, ... }:
+      {
+        imports = [
+          (import (getFilePath "test.nix") {
+            inherit inputs;
+            inherit name;
+            inherit lib;
+          })
+        ];
+        options = {
+          args = lib.mkOption {
+            description = "Additional arguments to pass to the test script";
+            type = lib.types.attrsOf lib.types.str;
+            default = { };
+          };
+        };
 
-      imports = [
-        (import (getFilePath "test.nix") {
-          inherit inputs;
+        config = {
           inherit name;
-          inherit lib;
-        })
-      ];
 
-      defaults = {
-        imports = [ defaultModule ];
+          defaults = {
+            imports = [ defaultModule ];
 
-        qois.outgoing-server-mail.enable = mkForce false;
-        qois.backup-client.enable = mkForce false;
-      };
+            qois.outgoing-server-mail.enable = mkForce false;
+            qois.backup-client.enable = mkForce false;
+          };
 
-      # Calls a `test(...)` python function in the test's python file with the list of nodes and helper functions.
-      # Helper symbols may be added as function args when needed and can be found in:
-      #   https://github.com/NixOS/nixpkgs/blob/master/nixos/lib/test-driver/src/test_driver/driver.py#L121
-      testScript = mkDefault (
-        { nodes, ... }:
-        let
-          script = readFile (getFilePath "test.py");
-          nodeArgs = pipe nodes [
-            attrNames
-            (map (val: "${val}=${val}"))
-            (concatStringsSep ", ")
-          ];
-        in
-        ''
-          ${script}
-          test(${nodeArgs}, subtest=subtest)
-        ''
-      );
-    };
+          # Calls a `test(...)` python function in the test's python file with the list of nodes and helper functions.
+          # Helper symbols may be added as function args when needed and can be found in:
+          #   https://github.com/NixOS/nixpkgs/blob/master/nixos/lib/test-driver/src/test_driver/driver.py#L121
+          testScript = mkDefault (
+            { nodes, ... }:
+            let
+              script = readFile (getFilePath "test.py");
+              # test nodes are passed as Python variable refs; config.args as string literals.
+              nodeRefArgs = lib.genAttrs (attrNames nodes) lib.id;
+              allArgs = nodeRefArgs // config.args;
+              nodeArgs = concatStringsSep ", " (
+                lib.mapAttrsToList (
+                  k: v:
+                  if nodeRefArgs ? ${k} && !(config.args ? ${k}) then
+                    "${k}=${v}" # Python variable reference
+                  else
+                    ''${k}="${v}"'' # Python string literal
+                ) allArgs
+              );
+            in
+            ''
+              ${script}
+              test(${nodeArgs}, subtest=subtest)
+            ''
+          );
+        };
+      }
+    );
 in
 pipe modulesBaseDir [
   getSubDirs
