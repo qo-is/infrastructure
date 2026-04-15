690650e6efced6542cd0bb4ae26bc353a7cb2fdb
Fabian Hauser <fabian@fh2.ch>
2026-04-15 23:01:48 +0300

    Switch to UTC time on hosts

--- nixos-configurations/lindberg-webapps/default.nix
+++ nixos-configurations/lindberg-webapps/default.nix
@@ -10,9 +10,6 @@
 
   qois.system.virtual-machine.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "Europe/Amsterdam";
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
--- nixos-configurations/lindberg/default.nix
+++ nixos-configurations/lindberg/default.nix
@@ -15,9 +15,6 @@
 
   qois.system.physical.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "Europe/Amsterdam";
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
