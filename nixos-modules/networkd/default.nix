1523b01ff7cb34955b0aca1cd602ffaaa085c8d1
Fabian Hauser <fabian@fh2.ch>
2026-05-28 20:16:44 +0300

    fix(networkd): restore useNetworkd = false when option disabled
    mkIf wrapping caused srvos mkDefault true to win in test VMs,
    silently enabling systemd-networkd and breaking the static-page test.

--- nixos-modules/networkd/default.nix
+++ nixos-modules/networkd/default.nix
@@ -1,21 +1,22 @@
 { lib, config, ... }:
 let
-  inherit (lib) mkEnableOption mkIf;
+  inherit (lib) mkEnableOption mkIf mkMerge;
 in
 {
   options.qois.networkd.enable = mkEnableOption "systemd-networkd";
 
-  config = mkIf config.qois.networkd.enable {
-    networking.useNetworkd = true;
-
-    services.resolved = {
-      enable = true;
-      llmnr = "false";
-      fallbackDns = [
-        # dns.switch.ch
-        "130.59.31.248"
-        "130.59.31.251"
-      ];
-    };
-  };
+  config = mkMerge [
+    { networking.useNetworkd = config.qois.networkd.enable; }
+    (mkIf config.qois.networkd.enable {
+      services.resolved = {
+        enable = true;
+        llmnr = "false";
+        fallbackDns = [
+          # dns.switch.ch
+          "130.59.31.248"
+          "130.59.31.251"
+        ];
+      };
+    })
+  ];
 }
