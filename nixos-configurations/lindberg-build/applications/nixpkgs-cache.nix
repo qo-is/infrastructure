d18a7d9f089c51be5a801b1e7174a816a0221e76
Fabian Hauser <fabian@fh2.ch>
2026-08-20 17:02:19 +0300

    Disable unused nixpkgs-cache proxy

--- nixos-configurations/lindberg-build/applications/nixpkgs-cache.nix
+++ nixos-configurations/lindberg-build/applications/nixpkgs-cache.nix
@@ -1,7 +1,10 @@
 { config, ... }:
 {
   qois.nixpkgs-cache = {
-    enable = true;
+    # Disabled: unused since the substituter reference to nixpkgs-cache.qo.is
+    # was commented out in nixos-modules/system/default.nix. Causes TLS
+    # verify errors proxying to cache.nixos.org while running unused.
+    enable = false;
     dnsResolvers = [ config.qois.meta.network.virtual.lindberg-vms-nat.hosts.lindberg.v4.ip ];
   };
 }
