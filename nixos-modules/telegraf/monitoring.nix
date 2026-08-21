917b76f9c948fb5b8478d0f5345c77d917986539
Fabian Hauser <fabian@fh2.ch>
2026-08-21 23:27:05 +0300

    Report Forgejo build status of infrastructure main via telegraf

--- nixos-modules/telegraf/monitoring.nix
+++ nixos-modules/telegraf/monitoring.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 let
@@ -52,6 +53,17 @@ in
       type = str;
       default = "1m";
     };
+    buildStatus = mkOption {
+      type = listOf anything;
+      default = [
+        {
+          owner = "qo.is";
+          repo = "infrastructure";
+          branch = "main";
+        }
+      ];
+      description = "Forgejo repos/branches to report the latest combined commit status for.";
+    };
   };
 
   config = mkIf cfg.enable {
@@ -64,6 +76,31 @@ in
         method = "native";
         urls = [ host ];
       }) cfg.ping;
+
+      exec = map (b: {
+        commands = [
+          "${pkgs.writeShellScript "forgejo-build-status-${b.repo}-${b.branch}" ''
+            set -uo pipefail
+            response=$(${pkgs.curl}/bin/curl -sf "https://git.qo.is/api/v1/repos/${b.owner}/${b.repo}/commits/${b.branch}/status")
+            if [ $? -ne 0 ] || [ -z "$response" ]; then
+              echo 'forgejo_build_status,owner=${b.owner},repo=${b.repo},branch=${b.branch},state=unreachable value=1i'
+              exit 0
+            fi
+            state=$(echo "$response" | ${pkgs.jq}/bin/jq -r '.state // "unknown"')
+            case "$state" in
+              success) code=0 ;;
+              pending) code=1 ;;
+              warning) code=2 ;;
+              failure|error) code=3 ;;
+              *) code=4 ;;
+            esac
+            echo "forgejo_build_status,owner=${b.owner},repo=${b.repo},branch=${b.branch},state=$state value=''${code}i"
+          ''}"
+        ];
+        timeout = "10s";
+        interval = "5m";
+        data_format = "influx";
+      }) cfg.buildStatus;
     };
   };
 }
