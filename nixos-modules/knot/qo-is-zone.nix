313c6285174a783b19395dff28bf15c4a7b87caf
Fabian Hauser <fabian@fh2.ch>
2026-08-30 13:53:48 +0300

    Group mail records and use record helpers for meta hosts

--- nixos-modules/knot/qo-is-zone.nix
+++ nixos-modules/knot/qo-is-zone.nix
@@ -38,18 +38,10 @@ let
     concatMap
       (
         { fqdn, host }:
-        [
-          {
-            name = "${fqdn}.";
-            type = "A";
-            data = host.v4.ip;
-          }
-        ]
-        ++ optional (host.v6 != null) {
-          name = "${fqdn}.";
-          type = "AAAA";
-          data = host.v6.ip;
-        }
+        let
+          name = removeSuffix ".${domain}" fqdn;
+        in
+        [ (mkA name host.v4.ip) ] ++ optional (host.v6 != null) (mkAAAA name host.v6.ip)
       )
       (
         filter (entry: hasSuffix ".${domain}" entry.fqdn) (
@@ -111,18 +103,18 @@ in
         (mkA "ns1" plessur-ext.hosts.calanda.v4.ip)
 
         (mkA "" lindberg.v4.ip)
-        (mkMX "" "0 mx.${domain}.")
-        (mkA "mx" "149.126.4.120")
-        (mkAAAA "mx" "2a01:ab20:0000:0004:0000:0000:0000:0120")
         (mkAAAA "stompert.eem-ext.net" "2a02:a449:0047:0001:020d:b9ff:fe4a:7a49")
 
         (mkCNAME "vpn" "calanda.plessur-ext.net.${domain}.")
         (mkCNAME "*.vpn" "vpn.${domain}.")
         (mkCNAME "*.vpn.net" "vpn.${domain}.")
-        (mkCNAME "autoconfig" "maildiscovery.cyon.ch.")
-        (mkCNAME "openpgpkey" "wkd.keys.openpgp.org.")
-        (mkSRV "_autodiscover._tcp" "0 0 443 maildiscovery.cyon.ch." // { ttl = 3600; })
 
+        (mkMX "" "0 mx.${domain}.")
+        (mkA "mx" "149.126.4.120")
+        (mkAAAA "mx" "2a01:ab20:0000:0004:0000:0000:0000:0120")
+        (mkCNAME "autoconfig" "maildiscovery.cyon.ch.")
+        (mkSRV "_autodiscover._tcp" "0 0 443 maildiscovery.cyon.ch." // { ttl = 3600; })
+        (mkCNAME "openpgpkey" "wkd.keys.openpgp.org.")
         (mkTXT "" "v=spf1 include:spf.protection.cyon.net -all")
         (mkTXT "default._domainkey" "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvqdVBBTioPZafUcajXYogvOb76Dnnu7K3PQtjafKpM4Am40L17K5I1UjTnuxoaUTQYVQiAKWnI3LoaYPUTWBDxYITv2oiyr/RtBkmetMyGrk/Fvew9Ankp9T5H4gASZqUqg/AWKXTsrxL5AqfULAlfkdOidwxrxMoH7IOWmfkNCb/sPUbnEq3HqKoOpoBduVHLCO4sDj10zbYsK3X+2Ju+k1ANiMfrB6GWJ1zmVwEGE34q1ztbX9cWbkpu1JgvLXde9gFNW9b95egHPgaqwPYOwz8IjzhZCyK13PJIbOtePf8lJoCDYFJMZLE2iWrcfHKILPn9lipu9KKNL+M3yk6QIDAQAB;")
       ];
